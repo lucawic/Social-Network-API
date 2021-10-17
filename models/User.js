b1669806e501280377aa17e6057e548a1ae9d37c
lucawic <lucaswick@email.arizona.edu>
2021-10-17 16:35:41 -0700

    added models and index.js connect

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+
+        // use REGEX here to validate email
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+})
+
+module.exports = User; 
---
+++ models/index.js
@@ -0,0 +1,7 @@
+//require user and thoughts models for connections
+
+const Thoughts = require('./Thoughts');
+const Users = require('./Users');
+
+//export
+module.exports = { Users, Thoughts };
