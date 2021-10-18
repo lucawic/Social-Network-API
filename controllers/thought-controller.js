4f4d30086ffc0ff6ad6a3fc94798d5a23b0be5a6
lucawic <lucaswick@email.arizona.edu>
2021-10-17 18:23:49 -0700

    adding connections again:

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,12 +1,8 @@
-module.exports = thoughtController;
-
-const { User, Thought, Reaction } = require('../models');
+const { user, thought, reaction } = require('../models');
 
 const thoughtController = {
-
-    // GET /api/thoughts
     getAllThoughts(req, res) {
-        Thought.find({})
+        thought.find({})
         .populate({ path: 'reactions', select: '-__v' })
         .select('-__v')
         .then(dbThoughtData => res.json(dbThoughtData))
@@ -16,14 +12,13 @@ const thoughtController = {
         })
     },
 
-    // GET /api/thoughts/:id
     getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
+        thought.findOne({ _id: params.id })
         .populate({ path: 'reactions', select: '-__v' })
         .select('-__v')
         .then(dbThoughtData => {
             if (!dbThoughtData) {
-                res.status(404).json({message: 'No thought found with this id'});
+                res.status(404).json({message: 'thought not found with this id'});
                 return;
             }
             res.json(dbThoughtData);
@@ -34,13 +29,6 @@ const thoughtController = {
         });
     },
 
-    // POST /api/thoughts
-    // expected body:
-    // {
-    //     "thoughtText": "foo",
-    //     "username": "bar",  // should be a username that corresponds to a User instance
-    //     "userId": "baz"  // should be a userId that corresponds to the same User instance as username
-    // }
     createThought({ body }, res) {
         Thought.create(body)
         .then(dbThoughtData => {
@@ -51,7 +39,7 @@ const thoughtController = {
             )
             .then(dbUserData => {
                 if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id' });
+                    res.status(404).json({ message: 'thought not found with this id' });
                     return;
                 }
                 res.json(dbUserData);
@@ -61,13 +49,6 @@ const thoughtController = {
         .catch(err => res.status(400).json(err));
     },
 
-    // PUT /api/thoughts/:id
-    // expected body should include at least one of the following attributes:
-    // {
-    //     "thoughtText": "foo",
-    //     "username": "bar",  // should be a username that corresponds to a User instance
-    //     "userId": "baz"  // should be a userId that corresponds to the same User instance as username
-    // }
     updateThought({ params, body }, res) {
         Thought.findOneAndUpdate(
             { _id: params.id },
@@ -76,7 +57,7 @@ const thoughtController = {
         )
         .then(dbThoughtData => {
             if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id' });
+                res.status(404).json({ message: 'thought not found with this id' });
                 return;
             }
             res.json(dbThoughtData);
@@ -84,67 +65,23 @@ const thoughtController = {
         .catch(err => res.status(400).json(err));
     },
 
-
-    // DELETE /api/thoughts/:id
     deleteThought({ params }, res) {
-        // delete the thought
         Thought.findOneAndDelete({ _id: params.id })
         .then(dbThoughtData => {
             if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id'});
+                res.status(404).json({ message: 'thought not found with this id'});
                 return;
             }
-            // delete the reference to deleted thought in user's thought array
             User.findOneAndUpdate(
                 { username: dbThoughtData.username },
                 { $pull: { thoughts: params.id } }
             )
             .then(() => {
-                res.json({message: 'Successfully deleted the thought'});
+                res.json({message: 'thought deleted'});
             })
             .catch(err => res.status(500).json(err));
         })
         .catch(err => res.status(500).json(err));
-    },
+    }}
 
-    // POST /api/thoughts/:id/reactions
-    addReaction({ params, body }, res) {
-        Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $addToSet: { reactions: body } },
-            { new: true, runValidators: true }
-        )
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id' });
-                return;
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => res.status(500).json(err));
-    },
-
-    // DELETE /api/thoughts/:id/reactions
-    // expected body should include at least one of the following attributes:
-    // {
-    //     "reactionId": "baz"  // should be a reactionId in the specified Thought instance
-    // }
-    
-    deleteReaction({ params, body }, res) {
-        Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $pull: { reactions: { reactionId: body.reactionId } } },
-            { new: true, runValidators: true }
-        )
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought found with this id' });
-                return;
-            }
-            res.json({message: 'Successfully deleted the reaction'});
-        })
-        .catch(err => res.status(500).json(err));
-    },
-}
-
-module.exports = thoughtController;
+    module.exports = thoughtController;
